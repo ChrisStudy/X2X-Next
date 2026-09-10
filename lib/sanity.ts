b988a4beca83bd16f3db76e1aea69d63be9ae6bb
Chris Xiong <chrisxiongyj@hotmail.com>
2026-09-10 22:44:47 +0800

    Sanity for blog post

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@sanity/client'
-import { createImageUrlBuilder } from '@sanity/image-url'
+import imageUrlBuilder from '@sanity/image-url'
+import type { SanityImageSource } from '@sanity/image-url'
 export const client = createClient({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
@@ -7,8 +8,8 @@ export const client = createClient({
     useCdn: process.env.NODE_ENV === 'production',
 })
 
-const builder = createImageUrlBuilder(client)
-export const urlFor = (source: any) => builder.image(source)
+const builder = imageUrlBuilder(client)
+export const urlFor = (source: SanityImageSource) => builder.image(source)
 
 export type PostData = {
     id: string;
